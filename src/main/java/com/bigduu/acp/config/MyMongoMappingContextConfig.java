5063c26cd3619de6d5a6df8109463c64658c11fc
mugeng.du <mugeng.du@qq.com>
2019-12-31 17:46:04 +0800

    修正答案

--- src/main/java/com/bigduu/acp/config/MyMongoMappingContextConfig.java
+++ src/main/java/com/bigduu/acp/config/MyMongoMappingContextConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 /**
+ * mongo Data JPA 不在推荐自动创建索引 所以需要手动开启 但是这里似乎不行 还需要查看文档
  * @author mugeng.du
  */
 @Configuration
